bd73b8b6272e4b7426f90bc3d4b99515ae27fc09
Wafa <zaidanwafa@gmail.com>
2018-04-07 13:44:01 +0100

    Added an SQL file where we can share our sql code easily

---
+++ SQL/SQLCode.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  newwafasky
+ * Created: 07-Apr-2018
+ */
+
