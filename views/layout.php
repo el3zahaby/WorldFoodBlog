04468a2d566b208d8c78ca1901448bb98b48a79f
Wafa <zaidanwafa@gmail.com>
2018-04-15 17:54:59 +0100

    cleaning up

--- views/layout.php
+++ views/layout.php
@@ -6,11 +6,6 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!--<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico|Pangolin" >-->
-
-
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
@@ -52,7 +47,7 @@
 
                     <li> <a href='?controller=post&action=create' class="glyphicon glyphicon-edit"> Create Post </a></li>
                     <li>  <a href='?controller=user&action=register'><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href='?controller=user&action=login'><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                 </ul>
             </div>
         </nav>
