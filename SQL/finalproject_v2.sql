406fa76190e8007c5f33b7cbd21f1503c28375d2
Wafa <zaidanwafa@gmail.com>
2018-04-16 08:39:02 +0100

    latest sql version
    latest sql database version named "finalproject_v2
    It has the cuisine, post and username table.

--- SQL/finalproject_v2.sql
+++ SQL/finalproject_v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2018 at 03:49 PM
+-- Generation Time: Apr 16, 2018 at 07:36 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.0.23
 
@@ -21,8 +21,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `finalproject`
 --
-CREATE DATABASE IF NOT EXISTS `finalproject` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `finalproject`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cuisine`
+--
+
+CREATE TABLE `cuisine` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cuisine`
+--
+
+INSERT INTO `cuisine` (`id`, `name`) VALUES
+(1, 'Vegitarian'),
+(2, 'Middle Eastern'),
+(3, 'European'),
+(4, 'Asian'),
+(5, 'South Asian'),
+(6, 'South East Asian');
 
 -- --------------------------------------------------------
 
@@ -34,15 +55,33 @@ CREATE TABLE `post` (
   `id` int(11) NOT NULL,
   `title` varchar(250) DEFAULT NULL,
   `content` varchar(60000) DEFAULT NULL,
-  `image` varchar(250) DEFAULT NULL
+  `image` varchar(250) DEFAULT NULL,
+  `DateAdded` varchar(20) DEFAULT NULL,
+  `cuisine_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `post`
 --
 
-INSERT INTO `post` (`id`, `title`, `content`, `image`) VALUES
-(1, 'Prawns', '<p>Prawns</p>', 'uploads/Prawns.jpeg');
+INSERT INTO `post` (`id`, `title`, `content`, `image`, `DateAdded`, `cuisine_id`) VALUES
+(47, 'Sushi', '<p style=\"text-align: center;\"><span style=\"color: #008080;\"><span style=\"color: #33cccc;\">shushi</span></span></p>', 'uploads/Sushi.jpeg', '2018/04/13', 1),
+(49, 'Noodles', '<p style=\"text-align: center;\"><span style=\"color: #33cccc;\">HElloooooo</span></p>', 'uploads/Noodles.jpeg', '2018/04/13', 4),
+(50, 'hi', '<p>hi</p>', 'uploads/hi.jpeg', '2018/04/13', 2),
+(51, 'fufu', '<p>Sushi</p>', 'uploads/fufu.jpeg', '2018/04/15', NULL),
+(52, 'sadsa', '<p>sadsa</p>', 'uploads/sadsa.jpeg', '2018/04/15', NULL),
+(53, 'hi', 'hi', 'hi', '12121', 1),
+(54, 'new', '<p>ewew</p>', 'uploads/new.jpeg', '2018/04/15', NULL),
+(55, 'new', '<p>ewew</p>', 'uploads/new.jpeg', '2018/04/15', NULL),
+(56, 'dsfdsf', '<p>dfds</p>', 'uploads/dsfdsf.jpeg', '2018/04/15', NULL),
+(57, 'dsfdsf', '<p>dfds</p>', 'uploads/dsfdsf.jpeg', '2018/04/15', NULL),
+(58, 'ewrew', '<p>erfew</p>', 'uploads/ewrew.jpeg', '2018/04/15', NULL),
+(59, 'ewrew', '<p>dsfds</p>', 'uploads/ewrew.jpeg', '2018/04/15', NULL),
+(60, 'ewrew', '<p>dsfds</p>', 'uploads/ewrew.jpeg', '2018/04/15', 3),
+(61, 'xczx', '<p>xcxz</p>', 'uploads/xczx.jpeg', '2018/04/15', 1),
+(62, 'xczx', '<p>xcxz</p>', 'uploads/xczx.jpeg', '2018/04/15', 1),
+(63, 'xczx', '<p>xcxz</p>', 'uploads/xczx.jpeg', '2018/04/15', 1),
+(68, 'dsad', '<p>sdsa</p>', 'uploads/dsad.jpeg', '2018/04/15', 1);
 
 -- --------------------------------------------------------
 
@@ -86,17 +125,28 @@ INSERT INTO `username` (`id`, `username`, `password`, `email`, `typeid`, `approv
 (33, 'Testing', 'Testing', 'test@wafa.com', NULL, NULL, '2018-04-11 15:14:32', '2018-04-11 15:14:32'),
 (34, 'WafaZ', 'WafaZ', 'WafaZ@gmail.com', NULL, NULL, '2018-04-11 16:02:18', '2018-04-11 16:02:18'),
 (35, 'Wafaaa', 'Wafaaa', 'Wafaaaa@wafa.com', NULL, NULL, '2018-04-11 16:28:38', '2018-04-11 16:28:38'),
-(36, 'Wafa Zaidan', 'Zaidan', 'Wafa@wafa.com', NULL, NULL, '2018-04-11 16:46:50', '2018-04-11 16:46:50');
+(36, 'Wafa Zaidan', 'Zaidan', 'Wafa@wafa.com', NULL, NULL, '2018-04-11 16:46:50', '2018-04-11 16:46:50'),
+(37, 'Hello', 'Hello', 'Hwllo@hi.com', NULL, NULL, '2018-04-12 21:21:28', '2018-04-12 21:21:28'),
+(38, 'Hi', 'hello12', 'Hi@hi.com', NULL, NULL, '2018-04-12 21:22:00', '2018-04-12 21:22:00'),
+(41, 'wafazzz', 'wafazz', 'wafa@wafaaaa.com', NULL, NULL, '2018-04-15 17:40:02', '2018-04-15 17:40:02'),
+(42, 'hii', 'hihihi', 'hi@hi.com', NULL, NULL, '2018-04-15 17:51:04', '2018-04-15 17:51:04');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `cuisine`
+--
+ALTER TABLE `cuisine`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cuisine_id` (`cuisine_id`);
 
 --
 -- Indexes for table `username`
@@ -110,16 +160,32 @@ ALTER TABLE `username`
 --
 
 --
+-- AUTO_INCREMENT for table `cuisine`
+--
+ALTER TABLE `cuisine`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT for table `username`
 --
 ALTER TABLE `username`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`cuisine_id`) REFERENCES `cuisine` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
