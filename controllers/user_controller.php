52325ba5083349b9042d80a49fa73b78a5d6416f
Wafa <35741448+WafaZaidan@users.noreply.github.com>
2018-04-21 11:58:53 +0100

    Update user_controller.php

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -12,19 +12,22 @@ class UserController {
     }
 
     public function read() {
+       
+      // we expect a url of form ?controller=posts&action=show&id=x
+      // without an id we just redirect to the error page as we need the post id to find it in the database
+      if (!isset($_GET['id']))
+        return call('pages', 'error');
 
-        if (!isset($_GET['id']))
-            return call('pages', 'error');
-
-        try {
-            // we use the given id to get the correct post
-            $post = Post::find($_GET['id']);
-            require_once('views/cuisine/read.php');
-        } catch (Exception $ex) {
-            return call('pages', 'error');
-        }
+      try{
+      // we use the given id to get the correct post
+      $user = User::find($_GET['id']);
+      $postsForContributor = Post::PostsByContributor($_GET['id']);
+      require_once('views/users/read.php');
+      }
+ catch (Exception $ex){
+     return call('pages','error');
+ }
     }
-
     public function register() {
 
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -34,9 +37,12 @@ class UserController {
             User::add();
             require_once('index.php');
         }
-    }
 
-    public function login() {
+}
+
+
+        
+ public function login() {
 
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             require_once('views/users/login.php');
@@ -45,21 +51,55 @@ class UserController {
             User::login();
             require_once('index.php');
         }
-    }
+
 
     public function logout() {
 
 
 
-        User::logout();
+   
+public function logout() {
 
-        header("location:index.php");
-    }
-
-    public function userAccount() {
-
-
-       require_once('views/users/userAccount.php');
-    }
+            User::logout();
+   
+              header("location:index.php");
 
 }
+
+    
+ public function displayallusers() {
+      // we store all the posts in a variable
+      $users = User::allusers();
+      require_once('views/users/displayallusers.php');
+    }
+
+    
+
+    public function update() {
+        
+      if($_SERVER['REQUEST_METHOD'] == 'GET'){
+          if (!isset($_GET['id']))
+        return call('pages', 'error');
+
+        // we use the given id to get the correct post
+        $users = User::find($_GET['id']);
+      
+        require_once('views/users/update.php');
+        }
+      else
+          { 
+            $id = $_GET['id'];
+            Post::update($id);
+                        
+            $User = User::all();
+            require_once('views/users/readallusers.php');
+      }
+      
+    }
+ 
+
+
+
+}
+
+
