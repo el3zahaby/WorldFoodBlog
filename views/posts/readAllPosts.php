724b60c5696d1d2c945e35046354f5ae263a580b
Wafa <zaidanwafa@gmail.com>
2018-04-21 21:13:16 +0100

    Styled the reall all posts page
    Aligned buttons and posts in read all cuisines

--- views/posts/readAllPosts.php
+++ views/posts/readAllPosts.php
@@ -1,51 +1,52 @@
 <html>
-    
-<body>
-   <style>
-    div.a {
-        text-align: center;
-    }
-    div.container{
-        margin: 1px;
-    }
+
+    <body>
+        <style>
+            div.a {
+                text-align: center;
+            }
+            div.container{
+                margin: 1px;
+            }
 
 
-</style>
-<div  class = "container" >
-   <p>Here is a list of all posts:</p>
+        </style>
+        <div  class = "container" > <div class="col-md-7"> 
+                <h4>Here is a list of all posts:</h4></div>
+            <div claiss="container">
+                <table class="RecipesTable">
+                    <tr>
 
 
+                        <?php foreach ($posts as $post) { ?>
+                        <p>
 
-<?php foreach($posts as $post) { ?>
-  <p>
+                        <tr>    <td>      <?php echo "<a href='?controller=post&action=read&id=" . $post->id . "'><img src=" . $post->image . ' width="150" height="100"/> </a>' ?></td> 
+                            <td>   <?php echo $post->title; ?> </td>
+                            <td> <a class="btn btn-secondary active"  href='?controller=post&action=read&id=<?php echo $post->id; ?>'>Read more</a> </td>
+                            <?php if (isset($_SESSION['username'])) { ?>
+                                <td>       <button class="btn btn-secondary active" value = "Delete" onclick="ConfirmDelete()">Delete Post</button> </td>
+                                <td>    <a class="btn btn-secondary active" href='?controller=post&action=update&id=<?php echo $post->id; ?>> '>Update Post</a> </td>
+                            <?php } ?> 
+                            </p>
 
-   <div claiss="container">
-  <?php  echo "<a href='?controller=post&action=read&id=". $post->id  ."'><img src=" . $post->image. ' width="150" height="100"/> </a>'?> &nbsp; &nbsp;
-<?php echo $post->title; ?> &nbsp; &nbsp;
+                            </div>
+                        <?php } ?>
+                    <script>
+                        function ConfirmDelete() {
+                            var txt;
+                            var r = confirm("Are you sure you want to delete this post?");
+                            if (r == true) {
+                                txt = "You pressed OK! ";
+                                window.location.href = ("?controller=post&action=delete&id=<?php echo $post->id; ?>");
+                            } else {
+                                txt = "You pressed Cancel!";
+                            }
+                            document.getElementById("demo").innerHTML = txt;
+                        }
+                    </script>
+                    </tr> </table>
+            </div>
+    </body>
 
-    <a class="btn btn-secondary active"  href='?controller=post&action=read&id=<?php echo $post->id; ?>'>Read more</a> &nbsp; &nbsp;
-    <?php
-    if (isset($_SESSION['username'])) {   ?>
-    <button class="btn btn-secondary active" value = "Delete" onclick="ConfirmDelete()">Delete Post</button> &nbsp; &nbsp;
-    <a class="btn btn-secondary active" href='?controller=post&action=update&id=<?php echo $post->id; ?>> '>Update Post</a> &nbsp;
-    <?php }?> 
-  </p>
- 
-</div>
-<?php } ?>
-<script>
-function ConfirmDelete() {
-    var txt;
-    var r = confirm("Are you sure you want to delete this post?");
-    if (r == true) {
-        txt = "You pressed OK! ";
-        window.location.href = ("?controller=post&action=delete&id=<?php echo $post->id; ?>");
-    } else {
-        txt = "You pressed Cancel!";
-    }
-    document.getElementById("demo").innerHTML = txt;
-}
-</script>
-</div>
-</body>
 </html>
