a29d58698e0516572f7409659cddaf558fd71e2d
Wafa <zaidanwafa@gmail.com>
2018-04-23 15:22:23 +0100

    confirm password field using ajax

--- views/users/registeruser.php
+++ views/users/registeruser.php
@@ -1,58 +1,120 @@
 <html >
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Sign up </title>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Sign up </title>
 
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
-</head>
-<body>
-<div class="container">
-     <div class="form-container">
-        <form method="post">
-            <h2>World Food Blog:Sign up.</h2><hr />
-            <?php
-            if(isset($error))
-            {
-               foreach($error as $error)
-               {
-                  ?>
-                  <div class="alert alert-danger">
-                      <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
-                  </div>
-                  <?php
-               }
-            }
-           // else if(isset($_GET['joined']))
-            {
-                 ?>
-                
-                 <?php
-            }
-            ?>
-            <div class="form-group">
+        <style type="text/css">
+            body{ font: 14px sans-serif; text-align: center; }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="form-container">
+                <form method="post">
+                    <h2>World Food Blog:Sign up.</h2><hr />
+                    <?php
+                    if (isset($error)) {
+                        foreach ($error as $error) {
+                            ?>
+                            <div class="alert alert-danger">
+                                <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                            </div>
+                            <?php
+                        }
+                    }
+// else if(isset($_GET['joined']))
+                    {
+                        ?>
 
-            <input type="text" class="form-control" name="username" placeholder="Enter Username" required value="<?php if(isset($error)){echo $username;}?>" />
-            </div>
-            <div class="form-group">
-            <input type="text" class="form-control" name="email" placeholder="Enter E-Mail" required value="<?php if(isset($error)){echo $email;}?>" />
+                        <?php
+                    }
+                    ?>
+                    <div class="form-group">
+
+                        <input type="text" class="form-control" name="username" placeholder="Enter Username" required value="<?php
+                        if (isset($error)) {
+                            echo $username;
+                        }
+                        ?>" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="email" placeholder="Enter E-Mail" required value="<?php
+                        if (isset($error)) {
+                            echo $email;
+                        }
+                        ?>" />
+
+                    </div>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+                    <div class="form-group">
+                        <input type="password" id="password" class="form-control" name="password"required  placeholder="Enter Password" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="confirm_password" id="confirm_password" class="form-control"required  placeholder="Confirm Password" />
+                    </div>
+
+
+
+
+
+
+
+                    <div class="clearfix"></div><hr />
+                    <div class="form-group">
+<!--                        <button id= 'submit' type="submit" class="btn btn-default" >-->
+                            <i class="glyphicon glyphicon-open-file"></i>&nbsp;SIGN UP
+                        </button>
+                    </div>
+                    <br />
+
+                </form>
+                <form name="formname">
+                    Enter password, 4 characters or more:<br>
+                    <input type="text" id="password1" onKeyUp="check_pass()"><br>
+                    Confirm password:<br>
+                    <input type="text" id="password2" onKeyUp="check_pass()"><br>
+                    <input type="submit" value="Submit" id="submit" disabled>
+                    <span id='message'></span>
+                </form>
 
             </div>
-            <div class="form-group">
-             <input type="password" class="form-control" name="password"required  placeholder="Enter Password" />
-            </div>
-            <div class="clearfix"></div><hr />
-            <div class="form-group">
-             <button type="submit" class="btn btn-default">
-                 <i class="glyphicon glyphicon-open-file"></i>&nbsp;SIGN UP
-                </button>
-            </div>
-            <br />
-            
-        </form>
-       </div>
-</div>
+        </div>
+        <script>
+//            $('#password, #confirm_password').on('keyup', function validate() {
+//                if ($('#password').val() == $('#confirm_password').val()) {
+//                  
+//                    $('#message').html('Matching').css('color', 'green');
+//                  $('#submit').prop('disabled', true);
+//                   
+//                } else
+//                    $('#message').html('Not Matching').css('color', 'red');
+//                 
+//                   $('#submit').prop('disabled', false);
+//            });
 
-</body>
+//            function validate() {
+//                isValid = (document.formname.password1.value == document.formname.password2.value)
+//                        && document.formname.password1.value.length > 3; // Add any additional validation here
+//                document.formname.submitbtn.disabled = !isValid;
+//
+//            }
+function check_pass() {
+    if (document.getElementById('password1').value ==
+            document.getElementById('password2').value) {
+        document.getElementById('submit').disabled = false;
+
+                   $('#message').html('Matching').css('color', 'green');
+    } else {
+        document.getElementById('submit').disabled = true;
+                      $('#message').html('Not Matching').css('color', 'red');
+    }
+}
+
+        </script>
+
+
+
+    </body>
 </html>
+
